afe65836dcbdf99a3436cbd2d893cce83d65babb
Mark Thomas <18529851+Meliv@users.noreply.github.com>
2024-03-23 21:39:34 +0000

    DI (sort of)

---
+++ CodeAnalyzer/Analyzer.cs
@@ -0,0 +1,21 @@
+﻿using CodeAnalyzer.Models;
+using CodeAnalyzer.Repositories;
+
+namespace CodeAnalyzer;
+
+public class Analyzer
+{
+    private readonly IContactRepository _contactRepository;
+
+    public Analyzer(IContactRepository contactRepository)
+    {
+        _contactRepository = contactRepository;
+    }
+
+    public void Invoke()
+    {
+        Contact contact = _contactRepository.Get();
+        
+        Console.Write($"Name: {contact.Name}");
+    }
+}
--- CodeAnalyzer/Program.cs
+++ CodeAnalyzer/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using CodeAnalyzer;
+using CodeAnalyzer.Repositories;
+using CodeAnalyzer.Repositories.Internals;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        ServiceProvider serviceProvider = new ServiceCollection()
+            .AddTransient<IContactRepository, ContactRepository>()
+            .BuildServiceProvider();
+
+        IContactRepository contactRepository = serviceProvider.GetRequiredService<IContactRepository>();
+
+        Analyzer analyzer = new Analyzer(contactRepository);
+
+        analyzer.Invoke();
     }
 }
